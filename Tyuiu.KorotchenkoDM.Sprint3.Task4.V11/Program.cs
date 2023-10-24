0072b4a6509da0f6aaf6f0dd8e197fca17dee3c8
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-10-24 18:10:04 +0500

    Task4

---
+++ Tyuiu.KorotchenkoDM.Sprint3.Task4.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KorotchenkoDM.Sprint3.Task4.V11.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint3.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Коротченко Д. М. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Коротченко Данил Михайлович | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y = x / (sin(x) - x) + 2                                        *");
+            Console.WriteLine("* При х = 0 пропустить значение. Полученные значения перемножить          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
