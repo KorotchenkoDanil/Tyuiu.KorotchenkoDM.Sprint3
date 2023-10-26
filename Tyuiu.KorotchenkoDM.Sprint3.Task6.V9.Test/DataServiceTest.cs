e05392ddef4746bc44c295285e150594240d5847
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-10-26 20:57:17 +0500

    Task6

---
+++ Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 14;
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            double wait = 18.565;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorotchenkoDM.Sprint3.Task6.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10f08822-680c-4f18-b823-050aba40aebd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
