711d83f3fb33870cdfcfb4a7f9a76186b400c2a7
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-11-18 10:56:36 +0500

    Task3

--- Tyuiu.KorotchenkoDM.Sprint3.Task3.V18.Test/DataServiceTest.cs
+++ Tyuiu.KorotchenkoDM.Sprint3.Task3.V18.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.KorotchenkoDM.Sprint3.Task3.V18.Test
             DataService ds = new DataService();
             string value = "4n5nvf 56 bgy";
             char item = 'n';
-            string res = ds.ReplaceNumOnChar(str, item);
+            string res = ds.ReplaceNumOnChar(value, item);
             string wait = "nnnnvf nn bgy";
             Assert.AreEqual(wait, res);
         }
